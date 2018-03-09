f2c8604719fa46c499d857730169179f6b844565
Martin Folkers <hello@twobrain.io>
2018-03-09 20:48:13 +0100

    Applying PSR2 coding style

--- kirby-pep.php
+++ kirby-pep.php
@@ -6,20 +6,20 @@
  * @package   Kirby CMS
  * @author    S1SYPHOS <hello@twobrain.io>
  * @link      http://twobrain.io
- * @version   0.2.0-beta
+ * @version   0.3.0
  * @license   MIT
  */
 
-if(!c::get('plugin.kirby-pep')) return;
+if (c::get('plugin.kirby-pep', false)) {
+    // Loading ParsedownExtraPlugin
+    require_once __DIR__ . DS . 'vendor' . DS . 'ParsedownExtraPlugin.php';
 
-// Loading ParsedownExtraPlugin
-require_once __DIR__ . DS . 'vendor' . DS . 'ParsedownExtraPlugin.php';
+    // Loading settings & core
+    load([
+      'kirby\\plugins\\pep\\settings'     => __DIR__ . DS . 'core' . DS . 'settings.php',
+      'kirby\\plugins\\pep\\markdown'     => __DIR__ . DS . 'core' . DS . 'markdown.php'
+    ]);
 
-// Loading settings & core
-load([
-  's1syphos\\pep\\settings'     => __DIR__ . DS . 'core' . DS . 'settings.php',
-  's1syphos\\pep\\markdown'     => __DIR__ . DS . 'core' . DS . 'markdown.php'
-]);
-
-// Registering with Kirby's extension registry
-kirby()->set('component', 'markdown', 'S1SYPHOS\\PEP\\MARKDOWN');
+    // Registering with Kirby's extension registry
+    kirby()->set('component', 'markdown', 'Kirby\\Plugins\\PEP\\Markdown');
+}
